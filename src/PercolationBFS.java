7e919c13d0a35b021ace66df96fd770eef8af936
Edem Ahorlu <edem.ahorlu@duke.edu>
2020-04-22 23:57:09 -0400

    p5 done

---
+++ src/PercolationBFS.java
@@ -0,0 +1,63 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PercolationBFS extends PercolationDFSFast{
+
+    /**
+     * Initialize myGrid of size (size x size)
+     * @param size - size of myGrid
+     */
+    public PercolationBFS (int size){
+        super(size);
+    }
+
+    @Override
+    /**
+     * Uses breadth-first search in order to mark cells as FULL
+     * @param row is the row of cell
+     * @param column is the column of cell
+     */
+    protected void dfs(int row, int  col){
+
+        //Check if cell is in bounds
+        if (!inBounds(row, col)) return;
+
+        //Check if cell is Full or blocked
+        if (isFull(row, col) || !isOpen(row, col)) return;
+
+        //Mark cell as FULL
+        myGrid[row][col] = FULL;
+        int [] rowDev = {-1, 1, 0, 0};
+        int [] colDev = {0, 0, -1, 1};
+
+        //Create queue
+        Queue<Integer> queue = new LinkedList<>();
+        int value = (row * myGrid.length) + col;
+
+        //Add mapped value of cell to queue
+        queue.add(value);
+
+        //Check the four neighbors of cell, and if they are open and not full then mark them as FULL and add to queue
+        while (queue.size() != 0){
+
+            Integer temp = queue.remove();
+
+            for (int i = 0; i < rowDev.length; i++){
+
+                int rowtemp = (temp / myGrid.length) + rowDev[i];
+                int coltemp = (temp % myGrid.length) + colDev[i];
+
+                if (inBounds(rowtemp, coltemp)){
+
+                    if (isOpen(rowtemp, coltemp) && !isFull(rowtemp, coltemp)){
+
+                        myGrid[rowtemp][coltemp] = FULL;
+                        queue.add((rowtemp * myGrid.length) + coltemp);
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/PercolationDFSFast.java
@@ -0,0 +1,48 @@
+public class PercolationDFSFast extends PercolationDFS{
+
+    /**
+     * Initialize myGrid of size (size x size)
+     * @param size - size of myGrid
+     */
+    public PercolationDFSFast(int size){
+        super(size);
+    }
+
+    @Override
+    /**
+     * Determine if cell should be marked as full and if so, call dfs(row, col)
+     * @param row is the row of cell
+     * @param col is the column of cell
+     */
+    protected void updateOnOpen(int row, int col) {
+
+        //If cell is in top row, mark as full
+        if (row == 0){
+            dfs(row,col);
+            return;
+        }
+
+        //Check if any of its four neighbors are full: if at least one of them is, then cell should be marked as full
+        else {
+
+            int [] rowDev = {-1, 1, 0, 0};
+            int [] colDev = {0, 0, -1, 1};
+
+            for (int i = 0; i < rowDev.length; i++){
+
+                int rowtemp = row + rowDev[i];
+                int coltemp = col + colDev[i];
+
+                if (inBounds(rowtemp, coltemp)){
+
+                    if (isFull(rowtemp, coltemp)){
+                        dfs(row, col);
+                        return;
+                    }
+
+                }
+
+            }
+        }
+    }
+}
